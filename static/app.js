a232974038d92f858b9c117f383b294c06c4276e
Giovanni Rufino <giovannirufino@gmail.com>
2021-06-05 11:07:43 -0400

    Completed DOM manipulation connection

--- static/app.js
+++ static/app.js
@@ -2,16 +2,18 @@ const $submit = $('#submit-guess')
 
 
 
-$submit.on('click', function(evt){
-    
+$submit.on('click', function(evt){    
     evt.preventDefault();
     const $guess = $('#guess').val();
+    if ( $guess.length < 3 ){
+        return;
+    }
     checkGuess($guess);
     $('#guess').val('');
 })
 
 async function checkGuess(guess){
-    console.log(guess)
-    response = await axios.get(`/find?guess=${guess}`)
-    console.log(response)
+    response = await axios.get(`/find?guess=${guess}`);
+    guessResult = response.data.result;
+    $('#result').text(guessResult);
 }
