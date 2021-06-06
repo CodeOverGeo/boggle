2051bcea688cef137ee1962871028b59223c9200
Giovanni Rufino <giovannirufino@gmail.com>
2021-06-06 13:40:19 -0400

    finished main functionality

--- app.py
+++ app.py
@@ -8,16 +8,33 @@ app.config['SECRET_KEY'] = 'checkers'
 
 toolbar = DebugToolbarExtension(app)
 
-board = boggle_game.make_board()
+times_played = 0
 
 @app.route('/')
 def show_game():
+    board = boggle_game.make_board()
     session['board'] = board
-    return render_template('index.html', board=board)
+    session.get('high_score', 0)
+    times_played = session.get('timesPlayed', 0)
+    return render_template('index.html', board = board, times_played = times_played)
 
 @app.route('/find')
 def check_guess():
     guess = request.args.get('guess')
+    board = session['board']
     result = boggle_game.check_valid_word(board, guess)
     reply = {'result': result}
+    return jsonify(reply)
+
+@app.route('/finish', methods=['POST'])
+def finished():
+    # import pdb
+    # pdb.set_trace()
+    
+    if session.get('high_score', 0) < request.json['highScore']:
+        session['high_score'] = request.json['highScore']
+    times_played = session.get('timesPlayed', 0)
+    times_played += 1
+    session['timesPlayed'] = times_played
+    reply = {'highScore': session['high_score'], 'timesPlayed': session['timesPlayed']}
     return jsonify(reply)
--- static/app.js
+++ static/app.js
@@ -1,25 +1,36 @@
-const $submit = $('#submit-guess')
+const $submit = $('#submit-guess');
+const $resetScore = $('#reset-score');
+const $start = $('#start');
+const $timer = $('#timer');
+const $letter = $('.letter');
+const $savedScore = $('#saved-score')
 const resultConverter = {
     'ok': 'Word is valid!', 
     'not-on-board': 'Word was not found on game board!', 
     'not-word': 'Guess is not a word!'};
 
-let score = 0
+let score = 0;
+let timer = 10;
+let countdownTimer = null;
+
+const guesses = [];
+
+$timer.text(timer)
 
 $submit.on('click', function(evt){    
     evt.preventDefault();
     const $guess = $('#guess').val();
-    if ( $guess.length < 3 ){
+    if (guesses.includes($guess) || $guess.length < 3){
         return;
     }
     runWord($guess);
     $('#guess').val('');
 })
 
-async function runWord(guess){
-    wordResult = await checkGuess(guess)
+async function runWord(guess){    
+    wordResult = await checkGuess(guess);
     updateHTML(wordResult);
-    updateScore(wordResult);
+    updateScore(wordResult, guess);
 }
 
 async function checkGuess(guess){
@@ -28,18 +39,53 @@ async function checkGuess(guess){
     guessResult = response.data.result;
     }
     catch(error){
-        console.error(error)
+        console.error(error);
     }
-    return guessResult
-    
+    return guessResult;
 }
 
-function updateHTML(word){    
+function updateHTML(word){  
     $('#result').text(resultConverter[word]);
 }
 
-function updateScore(word){
-    if (word = 'ok'){
-        console.log('updateScore')
+function updateScore(word, guess){
+    
+    if (word === 'ok'){
+        guesses.push(guess);
+        score += guess.length;
+        $('#score').text(`Score: ${score}`);
+        
     }
 }
+
+$resetScore.on('click', function(evt){
+    
+    score = 0;
+    $('#score').text(`Score: ${score}`);
+})
+
+async function saveResults(){
+    res = await axios.post('/finish', {highScore: score})
+    console.log(res)
+    timesPlayed = res.data.timesPlayed;
+    highScore = res.data.highScore;
+    $savedScore.html(`<p>Times Played:${timesPlayed}</p>
+    <p>High Score:${highScore}</p>`);
+}
+
+$start.on('click', function() {
+    $letter.css('color', 'black');
+    $start.off();
+    countdownTimer = setInterval(function(){
+    timer--
+    if(timer <= 0){
+        clearInterval(countdownTimer);
+        $submit.off();
+        $submit.on('click', (evt)=> evt.preventDefault());
+        $timer.text('TIMES UP!');
+        saveResults();
+    }else{
+        $timer.text(timer);
+    }    
+}, 1000);
+})
